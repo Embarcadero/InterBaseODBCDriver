eca746dec48cddc500a7627c20a98f6ca63ef98d
Sriram Balasubramanian <82998867+bsriramIdera@users.noreply.github.com>
2023-08-11 02:45:17 -0700

    Update build to 105. Added notes to IBchange.log
    Update build to 105. Added notes to IBchange.log

--- WriteBuildNo.h
+++ WriteBuildNo.h
@@ -1,5 +1,7 @@
 // Note - there must be two tabs between BUILDNUM_VERSION and
 // the actual number, otherwise the makefile for linux will not
 // pick up the value.
-#define BUILDNUM_VERSION		103
+// Check IBchange.log file for BUILDNUM_VERSION notes. Update then.
+// 
+#define BUILDNUM_VERSION		105
 
