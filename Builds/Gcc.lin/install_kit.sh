6d9151d1b04062665d4503ed4cf9f26be07657cb
Sriram Balasubramanian <82998867+bsriramIdera@users.noreply.github.com>
2024-06-20 16:20:50 -0700

    Linux and macOS install kit helper script for further testing
    Linux and macOS install kit helper script for further testing

--- Builds/Gcc.lin/install_kit.sh
+++ Builds/Gcc.lin/install_kit.sh
@@ -12,6 +12,5 @@ cp OdbcIB-1.0.0.107.tar.gz test
 cd test
 gunzip OdbcIB-1.0.0.107.tar.gz
 tar -xvf OdbcIB-1.0.0.107.tar
-#sudo -E ./install.sh --verbose --test --createTestDSN
-sudo -E ./install.sh --verbose --createTestDSN
+sudo ./install.sh
 
